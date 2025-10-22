764e6af2bb150f13aa476997e2e95d878f475720
sarahbeatriz0205 <sarah.nascimento02052007@gmail.com>
2025-10-21 21:35:38 -0300

    atividade em andamento

--- Atividades/Atividade1.py
+++ Atividades/Atividade1.py
@@ -14,21 +14,52 @@ class IPAdress:
         bin = int(bin)
         return bin
     
-    def conversao_mascara(self, mask):
-        mask = mask.conversao_binario(mask)
+    def ipv4_binario(self):
+        binIP = []
+        for byte in self.ip:
+            bin_endereco = self.conversao_binario(byte)
+            binIP.append(bin_endereco)
+        return binIP
+    
+    def mascara_binaria(self):
+        binMascara = []
+        for byte in self.mask:
+            bin_m = self.conversao_binario(byte)
+            binMascara.append(bin_m)
+        return binMascara
+
+    def conversao_mascara(self):
+        mask = self.mascara_binaria()
+        inversao = ""
+        lista_mascara_convertida = []
         for binarios in mask:
-            inversao = ""
             for b in binarios:
                 if b == "1":
                     b = "0"
+                    inversao = inversao + b
                 else:
                     b = "1"
-            inversao = inversao + b
-
-# binarioIP = []
-# binarioMask = []
-# for octeto in ip:
-#     binarioIP.append(conversao_binario(octeto))
-# for octeto in mask:
-#     binarioMask.append(conversao_binario(octeto))
+                    inversao = inversao + b
+            lista_mascara_convertida.append(inversao)
+        return lista_mascara_convertida
     
+    def endereco_de_rede(self):
+        endereco_rede = []
+        mask = self.mascara_binaria()
+        ip = self.ipv4_binario()
+        for i in range(len(ip)):
+            endereco = ip[i] & mask[i]
+        endereco_rede.append(endereco)
+        return endereco
+    
+    def broadcast(self):
+        endereco_broadcast = []
+        mask = self.conversao_mascara()
+        ip = self.ipv4_binario()
+        for i in range(len(ip)):
+            broadcast = (ip[i] | mask[i])
+            endereco_broadcast.append(broadcast)
+        return endereco_broadcast
+    
+
+# falta consertar o conversao_mascara() de str pra int
--- ex01.py
+++ ex01.py
@@ -1,6 +1,6 @@
 # ip = "10.9.0.222"
 # mask = "255.255.255.254"
-ip = list(map(int, input("IP = ").split(".")))
+ip = list(map(str, input("IP = ").split(".")))
 mask = list(map(int, input("Máscara = ").split(".")))
 
 binarioIP = []
@@ -21,14 +21,37 @@ for octeto in ip:
 for octeto in mask:
     binarioMask.append(conversao_binario(octeto))
 
-# for bin in mask:
-#     conversao_binario(mask[bin]) # erro
-#     inversao = ""
-#     for b in bin:
-#         if b == "1":
-#             b = "0"
-#         else:
-#             b = "1"
-#     inversao = inversao + b
 
-# print(inversao)
+concat = binarioMask[0] + binarioMask[1] + binarioMask[2] + binarioMask[3]
+print("Máscara concatenada = ", concat)
+
+ 
+def conversao_mascara(mask):
+        mask = mask.mascara_binaria()
+        for binarios in mask:
+            inversao = ""
+            for b in binarios:
+                if b == "1":
+                    b = "0"
+                else:
+                    b = "1"
+            inversao = inversao + b
+            return inversao
+print(conversao_mascara(mask))
+    
+binIP = []
+        
+def ipv4_binario(ip):
+    for byte in ip:
+        bin = conversao_binario(byte)
+        binIP.append(bin)
+    return binIP
+
+
+
+
+print("IP binário = ", *ipv4_binario(ip))
+
+# print(*binIP)
+
+
